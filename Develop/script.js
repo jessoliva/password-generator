65b3078e8a49fa265bd24fba3133c62bfc780d68
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-01-18 23:42:03 -0600

    password length prompt with conditions

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,22 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+function generatePassword () {
+  var confirmLength = parseInt(prompt("How many characters would you like your password to be? Input a number. Cannot be less than 8 or greater than 128."));
+
+  if (confirmLength >= 8 && confirmLength <= 128) {
+    console.log("User selected a password length = " + confirmLength);
+  }
+  else {
+    alert("Please enter a valid response.");
+    generatePassword();
+  }
+
+};
+
+
+
 
 // Write password to the #password input
 function writePassword() {
